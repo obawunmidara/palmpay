4c6c8c5414b1aeec9e150d8d2fedea4cd419907a
Dara <obawunmidara@gmail.com>
2024-09-21 21:18:45 -0700

    first commit

---
+++ palm.js
@@ -0,0 +1,430 @@
+
+let count = 0;
+let inp1 = document.getElementById("inp1");
+let inp2 = document.getElementById("inp2");
+let inp3 = document.getElementById("inp3");
+let inp4 = document.getElementById("inp4")
+function first(){
+    document.getElementById("inp1").value = '1'
+    if (inp1.value.length === 1){
+        document.getElementById("inp2").focus()
+    }
+}
+function second(){
+    document.getElementById("inp2").value = '1'
+    if (inp2.value.length === 1){
+        document.getElementById("inp3").focus()
+    }
+}
+function third(){
+    document.getElementById("inp3").value = '1'
+    if (inp3.value.length === 1){
+        document.getElementById("inp4").focus()
+    }
+}
+function fourth(){
+    document.getElementById("inp4").value = '1'
+    final()
+}
+
+function one(){
+    count++;
+    if(count===1){
+        first()
+    }
+    if(count===2){
+        second()
+    }
+    if(count===3){
+        third()
+    }
+    if(count===4){
+        fourth()
+    }
+}
+
+
+function first2(){
+    document.getElementById("inp1").value = '2'
+    if (inp1.value.length === 1){
+        document.getElementById("inp2").focus()
+    }
+}
+function second2(){
+    document.getElementById("inp2").value = '2'
+    if (inp2.value.length === 1){
+        document.getElementById("inp3").focus()
+    }
+}
+function third2(){
+    document.getElementById("inp3").value = '2'
+    if (inp3.value.length === 1){
+        document.getElementById("inp4").focus()
+    }
+}
+function fourth2(){
+    document.getElementById("inp4").value = '2'
+    final()
+}
+
+function two(){
+    count++;
+    if(count===1){
+        first2()
+    }
+    if(count===2){
+        second2()
+    }
+    if(count===3){
+        third2()
+    }
+    if(count===4){
+        fourth2()
+    }
+}
+
+function first3(){
+    document.getElementById("inp1").value = '3'
+    if (inp1.value.length === 1){
+        document.getElementById("inp2").focus()
+    }
+}
+function second3(){
+    document.getElementById("inp2").value = '3'
+    if (inp2.value.length === 1){
+        document.getElementById("inp3").focus()
+    }
+}
+function third3(){
+    document.getElementById("inp3").value = '3'
+    if (inp3.value.length === 1){
+        document.getElementById("inp4").focus()
+    }
+}
+function fourth3(){
+    document.getElementById("inp4").value = '3'
+    final()
+}
+
+function three(){
+    count++;
+    if(count===1){
+        first3()
+    }
+    if(count===2){
+        second3()
+    }
+    if(count===3){
+        third3()
+    }
+    if(count===4){
+        fourth3()
+    }
+}
+
+
+function first4(){
+    document.getElementById("inp1").value = '4'
+    if (inp1.value.length === 1){
+        document.getElementById("inp2").focus()
+    }
+}
+function second4(){
+    document.getElementById("inp2").value = '4'
+    if (inp2.value.length === 1){
+        document.getElementById("inp3").focus()
+    }
+}
+function third4(){
+    document.getElementById("inp3").value = '4'
+    if (inp3.value.length === 1){
+        document.getElementById("inp4").focus()
+    }
+}
+function fourth4(){
+    document.getElementById("inp4").value = '4'
+    final()
+}
+
+function four(){
+    count++;
+    if(count===1){
+        first4()
+    }
+    if(count===2){
+        second4()
+    }
+    if(count===3){
+        third4()
+    }
+    if(count===4){
+        fourth4()
+    }
+}
+function first5(){
+    document.getElementById("inp1").value = '5'
+    if (inp1.value.length === 1){
+        document.getElementById("inp2").focus()
+    }
+}
+function second5(){
+    document.getElementById("inp2").value = '5'
+    if (inp2.value.length === 1){
+        document.getElementById("inp3").focus()
+    }
+}
+function third5(){
+    document.getElementById("inp3").value = '5'
+    if (inp3.value.length === 1){
+        document.getElementById("inp4").focus()
+    }
+}
+function fourth5(){
+    document.getElementById("inp4").value = '5'
+    final()
+}
+
+function five(){
+    count++;
+    if(count===1){
+        first5()
+    }
+    if(count===2){
+        second5()
+    }
+    if(count===3){
+        third5()
+    }
+    if(count===4){
+        fourth5()
+    }
+}
+
+
+function first6(){
+    document.getElementById("inp1").value = '6'
+    if (inp1.value.length === 1){
+        document.getElementById("inp2").focus()
+    }
+}
+function second6(){
+    document.getElementById("inp2").value = '6'
+    if (inp2.value.length === 1){
+        document.getElementById("inp3").focus()
+    }
+}
+function third6(){
+    document.getElementById("inp3").value = '6'
+    if (inp3.value.length === 1){
+        document.getElementById("inp4").focus()
+    }
+}
+function fourth6(){
+    document.getElementById("inp4").value = '6'
+    final()
+}
+
+function six(){
+    count++;
+    if(count===1){
+        first6()
+    }
+    if(count===2){
+        second6()
+    }
+    if(count===3){
+        third6()
+    }
+    if(count===4){
+        fourth6()
+    }
+}
+function first7(){
+    document.getElementById("inp1").value = '7'
+    if (inp1.value.length === 1){
+        document.getElementById("inp2").focus()
+    }
+}
+function second7(){
+    document.getElementById("inp2").value = '7'
+    if (inp2.value.length === 1){
+        document.getElementById("inp3").focus()
+    }
+}
+function third7(){
+    document.getElementById("inp3").value = '7'
+    if (inp3.value.length === 1){
+        document.getElementById("inp4").focus()
+    }
+}
+function fourth7(){
+    document.getElementById("inp4").value = '7'
+    final()
+}
+
+function seven(){
+    count++;
+    if(count===1){
+        first7()
+    }
+    if(count===2){
+        second7()
+    }
+    if(count===3){
+        third7()
+    }
+    if(count===4){
+        fourth7()
+    }
+}
+function first8(){
+    document.getElementById("inp1").value = '8'
+    if (inp1.value.length === 1){
+        document.getElementById("inp2").focus()
+    }
+}
+function second8(){
+    document.getElementById("inp2").value = '8'
+    if (inp2.value.length === 1){
+        document.getElementById("inp3").focus()
+    }
+}
+function third8(){
+    document.getElementById("inp3").value = '8'
+    if (inp3.value.length === 1){
+        document.getElementById("inp4").focus()
+    }
+}
+function fourth8(){
+    document.getElementById("inp4").value = '8'
+    final()
+}
+
+function eigh(){
+    count++;
+    if(count===1){
+        first8()
+    }
+    if(count===2){
+        second8()
+    }
+    if(count===3){
+        third8()
+    }
+    if(count===4){
+        fourth8()
+    }
+}
+function first9(){
+    document.getElementById("inp1").value = '9'
+    if (inp1.value.length === 1){
+        document.getElementById("inp2").focus()
+    }
+}
+function second9(){
+    document.getElementById("inp2").value = '9'
+    if (inp2.value.length === 1){
+        document.getElementById("inp3").focus()
+    }
+}
+function third9(){
+    document.getElementById("inp3").value = '9'
+    if (inp3.value.length === 1){
+        document.getElementById("inp4").focus()
+    }
+}
+function fourth9(){
+    document.getElementById("inp4").value = '9'
+    final()
+}
+
+function nine(){
+    count++;
+    if(count===1){
+        first9()
+    }
+    if(count===2){
+        second9()
+    }
+    if(count===3){
+        third9()
+    }
+    if(count===4){
+        fourth9()
+    }
+}
+function first0(){
+    document.getElementById("inp1").value = '0'
+    if (inp1.value.length === 1){
+        document.getElementById("inp2").focus()
+    }
+}
+function second0(){
+    document.getElementById("inp2").value = '0'
+    if (inp2.value.length === 1){
+        document.getElementById("inp3").focus()
+    }
+}
+function third0(){
+    document.getElementById("inp3").value = '0'
+    if (inp3.value.length === 1){
+        document.getElementById("inp4").focus()
+    }
+}
+function fourth0(){
+    document.getElementById("inp4").value = '0'
+    final()
+}
+
+function zero(){
+    count++;
+    if(count===1){
+        first0()
+    }
+    if(count===2){
+        second0()
+    }
+    if(count===3){
+        third0()
+    }
+    if(count===4){
+        fourth0()
+    }
+}
+
+
+
+function final(){
+    if(inp1.value=== "1" && inp2.value=== "9" && inp3.value=== "7" && inp4.value=== "3"){
+        alert("Login successful")
+    }
+    else{
+        alert("TF niggur????????????")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
